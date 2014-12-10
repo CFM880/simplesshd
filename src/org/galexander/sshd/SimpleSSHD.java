3bbf3ce0dcd2cb8c74fb3011061409551094a3a7
Greg Alexander <gitgreg@galexander.org>
2014-12-10 16:17:23 -0500

    a bare project, created with
    android create project --target android-7 --name SimpleSSHD --path `pwd` --package org.galexander.sshd -a SimpleSSHD

---
+++ src/org/galexander/sshd/SimpleSSHD.java
@@ -0,0 +1,15 @@
+package org.galexander.sshd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleSSHD extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
