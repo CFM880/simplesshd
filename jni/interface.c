762d44b1a415fa62c9377207bd86293095c548a2
Greg Alexander <gitgreg@galexander.org>
2014-12-14 16:20:49 -0500

    notes about what the jni interface will need to be like..

---
+++ jni/interface.c
@@ -0,0 +1,4 @@
+
+/* XXX - java-invocable function that starts listener */
+/* XXX - do i need a function to generate host keys? DROPBEAR_DELAY_HOSTKEY */
+/* XXX - a C-callable interface to get property strings from the java side (paths, etc) */
