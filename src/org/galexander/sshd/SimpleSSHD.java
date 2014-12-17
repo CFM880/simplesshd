76eb76fac46a3ae52b503a7cb11bc45d6029baf0
Greg Alexander <gitgreg@galexander.org>
2014-12-17 18:43:01 -0500

    get rid of the old prefs machinery

--- src/org/galexander/sshd/SimpleSSHD.java
+++ src/org/galexander/sshd/SimpleSSHD.java
@@ -15,8 +15,6 @@ import android.net.Uri;
 
 public class SimpleSSHD extends Activity
 {
-	private CheckBox onboot_view;
-	private EditText port_view, path_view, shell_view, home_view;
 	private Button startstop_view;
 
 	public void onCreate(Bundle savedInstanceState)
@@ -24,24 +22,17 @@ public class SimpleSSHD extends Activity
 		super.onCreate(savedInstanceState);
 		Prefs.init(this);
 		setContentView(R.layout.main);
-		onboot_view = (CheckBox)findViewById(R.id.onboot);
-		port_view = (EditText)findViewById(R.id.port);
-		path_view = (EditText)findViewById(R.id.path);
-		shell_view = (EditText)findViewById(R.id.shell);
-		home_view = (EditText)findViewById(R.id.home);
 		startstop_view = (Button)findViewById(R.id.startstop);
 	}
 
 	public void onResume() {
 		super.onResume();
-		load_prefs();
 		update_startstop();
 		SimpleSSHDService.activity = this;
 	}
 
 	public void onPause() {
 		SimpleSSHDService.activity = null;
-		save_prefs();
 		super.onPause();
 	}
 
@@ -76,40 +67,10 @@ public class SimpleSSHD extends Activity
 	}
 
 	public void startstop_clicked(View v) {
-		save_prefs();
 		Intent i = new Intent(this, SimpleSSHDService.class);
 		if (SimpleSSHDService.is_started()) {
 			i.putExtra("stop", true);
 		}
 		startService(i);
 	}
-
-	private void load_prefs() {
-		onboot_view.setChecked(Prefs.get_onboot());
-		port_view.setText(Integer.toString(Prefs.get_port()));
-		path_view.setText(Prefs.get_path());
-		shell_view.setText(Prefs.get_shell());
-		home_view.setText(Prefs.get_home());
-	}
-
-	private void save_prefs() {
-		SharedPreferences.Editor edit = Prefs.edit();
-		boolean b = onboot_view.isChecked();
-		if (b != Prefs.get_onboot()) { edit.putBoolean("onboot", b); }
-		int i;
-		try {
-			i = Integer.valueOf(port_view.getText().toString());
-		} catch (Exception e) {
-			i = 0;
-		}
-		if (i != Prefs.get_port()) { edit.putInt("port", i); }
-		String s = path_view.getText().toString();
-		if (!s.equals(Prefs.get_path())) { edit.putString("path", s); }
-		s = shell_view.getText().toString();
-		if (!s.equals(Prefs.get_shell())) { edit.putString("shell", s); }
-		s = home_view.getText().toString();
-		if (!s.equals(Prefs.get_home())) { edit.putString("home", s); }
-		edit.commit();
-	}
-
 }
