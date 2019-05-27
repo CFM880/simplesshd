e0151fa757abec649cccff326ca9faf7b55bea1f
Greg Alexander <gitgreg@galexander.org>
2019-05-26 21:46:27 -0400

    Now can start-on-boot on Oreo/Pie, with the caveat that start-on-boot requires foreground-service.

--- app/src/main/java/org/galexander/sshd/BootReceiver.java
+++ app/src/main/java/org/galexander/sshd/BootReceiver.java
@@ -3,13 +3,19 @@ package org.galexander.sshd;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.Context;
+import android.os.Build;
 
 public class BootReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Prefs.init(context);
 		if (Prefs.get_onboot()) {
-			context.startService(new Intent(context,
-					SimpleSSHDService.class));
+			Intent i = new Intent(context, SimpleSSHDService.class);
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+				/* Oreo won't allow a background service here */
+				context.startForegroundService(i);
+			} else {
+				context.startService(i);
+			}
 		}
 	}
 }
