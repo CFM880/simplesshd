83b5dd496d1ee5ec4919d811b28a8a198c72b07e
Greg Alexander <gitgreg@galexander.org>
2019-06-09 15:50:49 -0400

    If asked to start at boot in background on Oreo, give a toast so the user knows why it doesn't work.

--- app/src/main/java/org/galexander/sshd/BootReceiver.java
+++ app/src/main/java/org/galexander/sshd/BootReceiver.java
@@ -4,15 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.Context;
 import android.os.Build;
+import android.widget.Toast;
+
 
 public class BootReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Prefs.init(context);
 		if (Prefs.get_onboot()) {
 			Intent i = new Intent(context, SimpleSSHDService.class);
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-				/* Oreo won't allow a background service here */
+			if (Prefs.get_foreground()) {
 				context.startForegroundService(i);
+			} else if (Build.VERSION.SDK_INT >=
+					Build.VERSION_CODES.O) {
+				Toast.makeText(context,
+"SimpleSSHD cannot start background at boot since Oreo (see Settings).",
+					Toast.LENGTH_LONG).show();
 			} else {
 				context.startService(i);
 			}
