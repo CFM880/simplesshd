5a5cc34841b972dff2f89054f1fa2ad3fe58381b
Greg Alexander <gitgreg@galexander.org>
2019-06-15 23:44:38 -0400

    Use correct #define to disable using environment variable to supply password (not that it matters, since we don't build the db client).

--- dropbear/localoptions.h
+++ dropbear/localoptions.h
@@ -199,7 +199,7 @@ much traffic. */
  * note that it will be provided for all "hidden" client-interactive
  * style prompts - if you want something more sophisticated, use 
  * SSH_ASKPASS instead. Comment out this var to remove this functionality.*/
-#define DROPBEAR_PASSWORD_ENV 0
+#define DROPBEAR_USE_PASSWORD_ENV 0
 
 /* Define this (as well as ENABLE_CLI_PASSWORD_AUTH) to allow the use of
  * a helper program for the ssh client. The helper program should be
